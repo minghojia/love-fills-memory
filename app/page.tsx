9f86cb4030efe3a249a5b27071ca36399cb1cbc5
Mingho Jia <mingho.chia@gmail.com>
2026-06-19 14:55:10 -0700

    first nextjs/tsx site

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>媽媽失去的記憶，我們用愛填滿</h1>
+      <p>Love fills memory.</p>
+    </main>
+  );
+}
